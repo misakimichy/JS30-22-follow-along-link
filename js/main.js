3af78d21ce940c8ccbfe4a70eb20ad7477ee14a9
Misaki Koonce <misaki.koonce@gmail.com>
2019-10-24 22:26:41 -0700

    Add function to find the width of hovered a element.

---
+++ js/main.js
@@ -0,0 +1,18 @@
+(function(){
+    const triggers = document.querySelectorAll('a');
+    const highlight = document.createElement('span');
+    highlight.classList.add('highlight');
+    document.body.append(highlight);
+
+    const highlightLink = (e) => {
+        // figure out the width of hovered element
+        const linkCoords = e.currentTarget.getBoundingClientRect();
+        console.log(linkCoords);
+        highlight.style.width = `${linkCoords.width}px`;
+        highlight.style.height = `${linkCoords.height}px`;
+        highlight.style.transform = `translate(${linkCoords.left}px, ${linkCoords.top}px)`;
+    };
+
+
+    triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
+}());
